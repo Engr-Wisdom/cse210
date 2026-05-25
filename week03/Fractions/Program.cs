89629170f987ea55479da915dadc1282959477ec
Wisdom Effiong <wisdomeffiong2008@gmail.com>
2026-05-25 14:46:21 +0100

    Fractions

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -7,10 +7,6 @@ class Program
         Fraction fraction = new Fraction();
         Fraction wholeNumber = new Fraction(1);
         Fraction topBottom = new Fraction(1, 3);
-        // fraction.SetTop(4);
-        // fraction.SetBottom(8);
-        // Console.WriteLine(fraction.GetTop());
-        // Console.WriteLine(fraction.GetBottom());
 
         Console.WriteLine(topBottom.GetFractionString());
         Console.WriteLine(topBottom.GetDecimalValue());
